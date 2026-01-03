d2ddc8020bbb225477374ca088cffbc771b037b9
jiasheng <astrognss@gmail.com>
2026-01-03 16:30:11 -0500

    optimized startup time, around 100ms

--- nvim/lua/user/custom_plugins/colorscheme_cfg.lua
+++ nvim/lua/user/custom_plugins/colorscheme_cfg.lua
@@ -1,21 +1,7 @@
-local function cfg()
-    vim.cmd.colorscheme "catppuccin-macchiato"
-    require('catppuccin').setup({
-        integrations = {
-            aerial = true,
-            lsp_saga = false,
-        },
-    })
-end
-
-local M =
-{
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = cfg,
-}
-
 return {
-    M
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    }
 }
--- nvim/lua/user/lsp_cfg.lua
+++ nvim/lua/user/lsp_cfg.lua
@@ -119,33 +119,29 @@ local function setup_keymap()
             client.server_capabilities.semanticTokensProvider = nil
 
             -- All the keymaps
+            -- grn, rename
+            -- <C-S>, signature_help
+            -- gO, document_symbol
+            -- ]d, diagnostic next
+            -- [d, diagnostic prev
+            -- ]D, diagnostic last
+            -- [D, diagnostic first
+            -- <C-W>d, diagnostic open
+            -- [q, quickfix next
+            -- ]q, quickfix prev
+            -- [a, argument list next
+            -- ]a, argument list prev
             local opts = { noremap = true, silent = true }
             vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-            --    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<CR>", opts)
             vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
             vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wx', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
                 opts)
-            -- collisio with telescope
-            -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>',
-                opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>',
-                opts)
-            vim.api.nvim_buf_set_keymap(
-                bufnr,
-                "n",
-                "<leader>of",
-                '<cmd>lua vim.diagnostic.open_float()<CR>',
-                opts
-            )
             vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
             vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
 
